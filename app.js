eda3b0ae3303f0d551c78ebd60680dcc8d56da56
Aderauwu04 <aderabg1@gmail.com>
2021-10-08 00:58:09 -0400

    metodos iniciales creados

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require('express');
-var path = require('path');
+var path = require('path')
 var cookieParser = require('cookie-parser');
+var logger = require('morgan')
+
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+var routes = require("./routes/index")
+app.use(routes)
+
 app.listen(8080, () => {
     console.log("Servidor activo")
   });
-
-var routes = require("./routes/index")
-app.use('/Ciudad_de_Arepitas',routes)
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var router = express.Router()
 
+var casas = [
+    {
+    'pisos' : 2,
+    'ventanas' : 4
+  }
+]
+var edificios = [
+  {
+  'pisos' : 6,
+  'ventanas' : 70
+  },
+  {
+    'pisos' : 3,
+    'ventanas' : 10
+  }
+]
+
+let ciudad = casas.length + edificios.length;
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('hii')
+  res.render('index', {
+    'Tamaño' : ciudad,
+    'Casas' : casas.length,
+    'Edificios' :edificios.length
+  })
 });
 
-router.get('/get/params/:arepa', function(req, res) {
-  res.send('hii'+req.agregar.arepa)
-});
+router.post('/post/query/casas', function (req, res) {
+    casas.push({'pisos':req.query.pisos, 'ventanas': req.query.ventanas})
+    res.json(casas)
+})
+
+router.post('/post/query/edificios', function (req, res) {
+  edificios.push({'pisos':req.query.pisos, 'ventanas':req.query.ventanas})
+  res.json(edificios)
+})
+
+router.get('/casas', function (req, res) {
+  res.json(casas)
+})
+router.get('/edificios', function (req, res) {
+  res.json(edificios)
+})
+
+console.log("Tamaño total = ", ciudad, ". Casas = ", casas, ". Edificios = ", edificios)
 
 module.exports = router;
