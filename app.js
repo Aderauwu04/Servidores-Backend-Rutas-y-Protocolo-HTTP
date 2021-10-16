ac7a4d5f6b6bb074632ddca098feb1fec3aefbe6
Aderauwu04 <aderabg1@gmail.com>
2021-10-16 09:28:38 -0400

    Merge pull request #8 from Aderauwu04/issue-6-Crear_los_metodos
    Issue 6 crear los metodos

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
-var path = require('path');
+var path = require('path')
+var logger = require('morgan')
 
 var app = express();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-var assets = require(/routes/assets)
-app.uses("/assets, assets")
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-var routes = require(/routes/index)
-app.uses("/index, index")
+var routes = require("./routes/index")
+app.use(routes)
+
+app.listen(8080, () => {
+  console.log("Servidor activo en el puerto 8080")
+});
 
 module.exports = app;
